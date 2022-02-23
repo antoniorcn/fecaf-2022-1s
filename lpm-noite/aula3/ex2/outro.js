ee7d9b8befd87517bc737a89586d850e27520d11
Antonio Carvalho <antoniorcn@hotmail.com>
2022-02-22 22:11:54 -0300

    Aula 3 LPM Noite

---
+++ lpm-noite/aula3/ex2/outro.js
@@ -0,0 +1,8 @@
+var numero1 = 10;
+var numero2 = 45;
+
+var soma = numero1 + numero2;
+
+
+console.log("A soma foi ");
+console.log(soma);
---
+++ lpm-noite/aula3/ex2/variaveis.js
@@ -0,0 +1,10 @@
+var nomeEmpresa = "FECAF";
+// Camel Case
+var salarioRecebidoFuncMesAtual;
+
+console.log("Declarei as variaveis");
+
+console.log("Exercicio 2");
+console.log("Nome da Empresa: ", nomeEmpresa);
+
+console.log("fim do programa");
