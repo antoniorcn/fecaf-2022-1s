6ac77ebb9f5a77a5157b63d0922ca0d0fdc81eef
Antonio Carvalho <antoniorcn@hotmail.com>
2022-03-21 08:03:38 -0500

    Aula 5

--- pdmii-manha/aula5/calculadora.js
+++ pdmii-manha/aula5/calculadora.js
@@ -4,8 +4,8 @@ import {Alert, Button, StyleSheet, Text, TouchableHighlight, View} from 'react-n
 
 const Botao = (props) => { 
   return ( 
-    <TouchableHighlight onPress={{
-
+    <TouchableHighlight onPress={()=>{
+      props.mudar(props.title);
     }}>
       <View {...props}>
         <Text style={{textAlign: "center"}}>{props.title}</Text>
@@ -16,37 +16,37 @@ const Botao = (props) => {
 
 export default () => { 
 
-  
+  const [valor, setValor] = useState(0);
 
   return (
     <View style={{flexDirection: "column", flex: 1}}>
       <View style={{flexDirection: "row", flex: 1}}>
-        <Text style={{flex: 4}}>0</Text>
-        <Botao style={estilo.botao} title="CE"/>
+        <Text style={{flex: 4}}>{valor}</Text>
+        <Botao style={estilo.botao} title="CE" mudar={setValor}/>
       </View>
       <View style={{flexDirection: "row", flex: 1}}>
-        <Botao title="1" style={estilo.botao}/>
-        <Botao title="2" style={estilo.botao}/>
-        <Botao title="3" style={estilo.botao}/>
-        <Botao title="+" style={estilo.botao}/>
+        <Botao title="1" style={estilo.botao} mudar={setValor}/>
+        <Botao title="2" style={estilo.botao} mudar={setValor}/>
+        <Botao title="3" style={estilo.botao} mudar={setValor}/>
+        <Botao title="+" style={estilo.botao} mudar={setValor}/>
       </View>
       <View style={{flexDirection: "row", flex: 1}}>
-        <Botao title="4" style={estilo.botao}/>
-        <Botao title="5" style={estilo.botao}/>
-        <Botao title="6" style={estilo.botao}/>
-        <Botao title="-" style={estilo.botao}/>
+        <Botao title="4" style={estilo.botao} mudar={setValor}/>
+        <Botao title="5" style={estilo.botao} mudar={setValor}/>
+        <Botao title="6" style={estilo.botao} mudar={setValor}/>
+        <Botao title="-" style={estilo.botao} mudar={setValor}/>
       </View>
       <View style={{flexDirection: "row", flex: 1}}>
-        <Botao title="7" style={estilo.botao}/>
-        <Botao title="8" style={estilo.botao}/>
-        <Botao title="9" style={estilo.botao}/>
-        <Botao title="*" style={estilo.botao}/>
+        <Botao title="7" style={estilo.botao} mudar={setValor}/>
+        <Botao title="8" style={estilo.botao} mudar={setValor}/>
+        <Botao title="9" style={estilo.botao} mudar={setValor}/>
+        <Botao title="*" style={estilo.botao} mudar={setValor}/>
       </View>            
       <View style={{flexDirection: "row", flex: 1}}>
-        <Botao title="." style={estilo.botao}/>
-        <Botao title="0" style={estilo.botao}/>
-        <Botao title="=" style={estilo.botao}/>
-        <Botao title="/" style={estilo.botao}/>
+        <Botao title="." style={estilo.botao} mudar={setValor}/>
+        <Botao title="0" style={estilo.botao} mudar={setValor}/>
+        <Botao title="=" style={estilo.botao} mudar={setValor}/>
+        <Botao title="/" style={estilo.botao} mudar={setValor}/>
       </View>                  
     </View>
   )
