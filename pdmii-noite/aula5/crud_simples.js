230f3e8915fab401ad3aafbdca18b8f4020da2c8
Antonio Carvalho <antoniorcn@hotmail.com>
2022-03-21 21:54:44 -0300

    Aula 5 parte 2

--- pdmii-noite/aula5/crud_simples.js
+++ pdmii-noite/aula5/crud_simples.js
@@ -6,9 +6,10 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 const Tab = createBottomTabNavigator();
 
 const Formulario = () => { 
-    const v = useState("");
-    const nome = v[0];
-    const setNome = v[1];
+    // const v = useState("");
+    // const nome = v[0];
+    // const setNome = v[1];
+    const [nome, setNome] = useState("");
     return (
         <View>
             <Text>Nome:</Text>
@@ -16,29 +17,44 @@ const Formulario = () => {
                 onChangeText={(txt)=>{
                     setNome(txt);
                 }}/>
-            <Button title="Gravar"/>
+            <Button title="Gravar" 
+                onPress={() => { 
+                    const lista = {...props.lista};
+                    lista.push(nome);
+                    props.setLista(lista);
+                }}/>
         </View>
     );
 }
 
 const Listar = () => { 
+    const listaDisplay = [];
+    for (let i=0; i < props.lista.lenght; i++) { 
+        listaDisplay.push(<Text>{props.lista[i]}</Text>);
+    }
     return (
         <View>
             <Text></Text>
-            <Text>João Silva</Text> 
+            {listaDisplay}
         </View>
     );
 }
 
 export default () => { 
+    const [nomes, setNomes] = useState([]);
     return (
         <View style={{flex: 1}}>
             <NavigationContainer>
                 <Tab.Navigator>
-                  <Tab.Screen name="Cadastro" component={Formulario}/>
-                  <Tab.Screen name="Listar" component={Listar}/>          
+                  <Tab.Screen name="Cadastro">
+                    {() => <Formulario {...this.props} 
+                                setLista={setNomes} lista={nomes}/>} 
+                  </Tab.Screen>
+                  <Tab.Screen name="Listar">          
+                    {() => <Listar {...this.props} lista={nomes}/>}
+                  </Tab.Screen>
                 </Tab.Navigator>
             </NavigationContainer>
         </View>
     );
-}
+}https://github.com/antoniorcn/fecaf-2022-1s
